eb34cf10ae7db4c6f754cf481c59a6345c6c19ae
EyobABN <eyob.alamerew.belay@gmail.com>
2022-08-07 14:57:10 +0300

    Add base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+    BaseModel module defining the BaseModel class
+"""
+
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    """
+        a class that defined all common attributes/methods for other classes
+    """
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        return '[' + self.__class__.__name__ + '] (' + self.id + ') ' + str(self.__dict__)
+
+    def save(self):
+        """
+            updates updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+            returns a dictionary containing all keys/values of the instance's __dict__
+        """
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
