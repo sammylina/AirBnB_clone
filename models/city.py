b72c844c91802c4636042c10f64e133e9b3eb019
EyobABN <eyob.alamerew.belay@gmail.com>
2022-08-07 14:57:33 +0300

    Add city.py

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    module for City
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+        Class for City
+    """
+    state_id = ""
+    name = ""
