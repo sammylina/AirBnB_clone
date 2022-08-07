0a4a738ea0f3b7cbf6594c4c398186c67a4de36f
EyobABN <eyob.alamerew.belay@gmail.com>
2022-08-07 14:57:27 +0300

    Add amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    module for Amenity
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+        Class for Amenity
+    """
+    name = ""
